07318e8935f289abc99c5c0e6d478c5f241ad4ca
Faith Magras <bdanielle092@gmail.com>
2020-10-05 12:05:04 -0500

    working on chapter 7

--- Gifter/client/src/components/PostForm.js
+++ Gifter/client/src/components/PostForm.js
@@ -1,88 +1,92 @@
-import React, {useState} from "react";
-import "../Posts/Post.css";
-import { Form, Col, Button, Row } from 'react-bootstrap';
-import React from 'react';
+import React, { useState, useContext } from "react";
+import {
+    Form,
+    FormGroup,
+    Card,
+    CardBody,
+    Label,
+    Input,
+    Button,
+} from "reactstrap";
+import { PostContext } from "../providers/PostProvider";
+import { useHistory } from "react-router-dom";
 
+const PostForm = () => {
+    const { addPost } = useContext(PostContext);
+    const [userProfileId, setUserProfileId] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [title, setTitle] = useState("");
+    const [caption, setCaption] = useState("");
+    const [dateCreated, setDateCreate] = useState("")
 
-function PostForm(props) {
-    const [post, setPost] = useState({ userId: parseInt(sessionStorage.activeUserID), title: "", url: "", caption: "" });
-    let newVariable = props.placeConstruct;
+    // Use this hook to allow us to programatically redirect users
+    const history = useHistory();
 
-    const handleFieldChange = evt => {
-        const stateToChange = {...post}
-        stateToChange[evt.target.id] = evt.target.value;
-        setPost(stateToChange);
+    const submit = (e) => {
+        const post = {
+            imageUrl,
+            title,
+            caption,
+            userProfileId: +userProfileId,
+            dateCreated
+        };
+
+        addPost(post).then((p) => {
+            // Navigate the user back to the home route
+            history.push("/");
+        });
     };
 
-    const constructNewPost = evt => {
-        evt.preventDefault();
-        addPost(post).then(() => {
-            getAllPosts()
-        })
-    };
-
-   return (
-       <>
-       <select>
-           <button type="button"
-               className="btn"
-               onChange={handleFieldChange}>
-                   Add Post
-           </button>
-       </select>
-
-       <form>
-           <fieldset>
-               <div className="formgrid">
-                   <input
-                   type="text"
-                   required
-                   onChange={handleFieldChange}
-                   id="userId"
-                   placeholder="Post UserId"
-                   />
-                   <label hmtlFor="UserId">UserId</label>
-
-                   <input
-                   type="text"
-                   required
-                   onChange={handleFieldChange}
-                   id="title"
-                   placeholder="Post Title"
-                   />
-                   <label hmtlFor="title">Title</label>
-
-                   <input
-                   type="text"
-                   required
-                   onChange={handleFieldChange}
-                   id="url"
-                   placeholder="Post Url"
-                   />
-                   <label hmtlFor="url">Url</label>
-
-                   <input
-                   type="text"
-                   required
-                   onChange={handleFieldChange}
-                   id="caption"
-                   placeholder="Post Caption"
-                   />
-                   <label hmtlFor="caption">Caption</label>
-                   
+    return (
+        <div className="container pt-4">
+            <div className="row justify-content-center">
+                <Card className="col-sm-12 col-lg-6">
+                    <CardBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="userId">User Id (For Now...)</Label>
+                                <Input
+                                    id="userId"
+                                    onChange={(e) => setUserProfileId(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="imageUrl">Gif URL</Label>
+                                <Input
+                                    id="imageUrl"
+                                    onChange={(e) => setImageUrl(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="title">Title</Label>
+                                <Input id="title" onChange={(e) => setTitle(e.target.value)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="caption">Caption</Label>
+                                <Input
+                                    id="caption"
+                                    onChange={(e) => setCaption(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="DateCreated">Date (For Now...)</Label>
+                                <Input
+                                    type="datetime-local"
+                                    id="dateCreated"
+                                    onChange={(e) => setDateCreate(e.target.value)}
+                                />
+                            </FormGroup>
 
 
-               </div>
-               <div>
-                   <button
-                   type="button"
-                   disable={isLoading}
-                   onClick={constructNewPost}
-                   >Submit</button>
-               </div>
-           </fieldset>
-       </form>
-       </>
-   )
-    
-}
+                        </Form>
+                        <Button color="info" onClick={submit}>
+                            SUBMIT
+            </Button>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default PostForm;
--- Gifter/client/src/providers/PostProvider.js
+++ Gifter/client/src/providers/PostProvider.js
@@ -3,28 +3,33 @@ import React, { useState } from "react";
 export const PostContext = React.createContext();
 
 export const PostProvider = (props) => {
-  // this will store the post which is stored in state
-  const [posts, setPosts] = useState([]);
-// created this function. This will go to the api that we created and get our post
-  const getAllPosts = () => {
-    return fetch("/api/post")
-      .then((res) => res.json())
-      .then(setPosts);
-  };
+    const [posts, setPosts] = useState([]);
 
-  const addPost = (post) => {
-    return fetch("/api/post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(post),
-    });
-  };
-// will make the post available to the rest of the app
-  return (
-    <PostContext.Provider value={{ posts, getAllPosts, addPost }}>
-      {props.children}
-    </PostContext.Provider>
-  );
+    const getAllPosts = () => {
+        return fetch("/api/post")
+            .then((res) => res.json())
+            .then(setPosts);
+    };
+
+    const addPost = (post) => {
+        return fetch("/api/post", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(post),
+        });
+    };
+
+    const getPost = (id) => {
+        return fetch(`/api/post/${id}`).then((res) => res.json());
+    };
+
+
+
+    return (
+        <PostContext.Provider value={{ posts, getAllPosts, addPost, getPost }}>
+            {props.children}
+        </PostContext.Provider>
+    );
 };
